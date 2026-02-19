e335dedee3a0c0e866e7f595099168c5593ef9fe
HiddenWaste <cartergordon13@gmail.com>
2026-02-18 23:46:40 -0600

    yt-dlp-setup

---
+++ BashScripts/yt-dlp-install-test.sh
@@ -0,0 +1,13 @@
+
+ 
+echo "Lets get our YouTube to MP3 Player!"
+
+snap install yt-dlp
+
+echo "Installation successfull? Make one more check"
+
+hash -r
+
+echo "Testing Strawberry Switchblade...."
+
+yt-dlp -x --audio-format mp3 https://youtu.be/x7QPBzAJ_io?si=o0t2W-ef8E-gF-dM
