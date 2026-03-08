e99fed932a9f218fd175d5173d61a11611818947
HiddenWaste <cartergordon13@gmail.com>
2026-03-07 22:27:42 -0600

    started vlc command

--- BashScripts/yt-dlp-install-test.sh
+++ BashScripts/yt-dlp-install-test.sh
@@ -13,3 +13,5 @@ echo "Testing Strawberry Switchblade...."
 yt-dlp -x --audio-format mp3 https://youtu.be/x7QPBzAJ_io?si=o0t2W-ef8E-gF-dM
 
 echo "All finished! Happy Sampling!!"
+
+# vlc ./'Strawberry Switchblade
