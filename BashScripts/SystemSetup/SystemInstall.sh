21782c0c4d1495c526f85961da5a267484282183
cartergordon13@gmail.com <cartergordon13@gmail.com>
2026-03-08 20:07:47 -0600

    SystemSetup additions, snap packages now work, found out about banner

--- BashScripts/SystemSetup/SystemInstall.sh
+++ BashScripts/SystemSetup/SystemInstall.sh
@@ -1,16 +1,53 @@
-sudo apt update
+sudo apt update > /dev/null 2>&1
+sudo apt upgrade > /dev/null 2>&1
 
-sudo apt install -y $(cat ../sudo-apt-pkgs.txt)
+# Banner first for *dramatic* purposes
+sudo apt install sysvbanner > /dev/null 2>&1
+
+banner "| System |"
+banner "| Starts |"
+
+echo "We shall begin with the apt packages...."
+sudo apt install -y $(cat ./pkg-lists/apt.txt)
 
 echo "Now for the snap packages!"
 
 # Snap installation
-xargs -a ../snap-packages.txt sudo snap install
+# xargs -a ./pkg-lists/snap.txt sudo snap install
 # With classic flag
+#xargs -a ./pkg-lists/snap-classic.txt sudo snap install --classic
+
+# More complex route, above would error with how it fed
+# the command was passed with all at once
+
+while IFS= read -r snap_pkg || [ -n "$snap_pkg" ]; 
+	do
+	
+		# Install the package
+		if snap list "$snap_pkg" >/dev/null 2>&1; then
+			echo ">> $snap_pkg already here!"
+		else
+			banner "$snap_pkg"
+			sudo snap install "$snap_pkg"
+		
+
+	#	if [ $? -ne 0 ]; then 
+	#		echo "$snap_pkg failed, next." 
+	#	fi
+
+		# Watch for it to finish
+			while pgrep -x "snap" > /dev/null; do sleep 1; done
+	fi
+done < ./pkg-lists/snap.txt 
+
+while IFS= read -r snap_pkg || [ -n "$snap_pkg"] 
+	do banner "(C) $snap_pkg"
+	sudo snap install "$snap_pkg" --classic
+done < ./pkg-lists/snap-classic.txt
 
 # Various dotfiles
 
-# Below is for terminal profiles
+## Gnome terminal profiles
 dconf load /org/gnome/terminal/legacy/profiles:/ < terminal_profiles.dconf
 
 echo "Rise up gamer."
