dab6882d5fa6a028df1f0b02c130049493dc7b47
carter.gordon@usiouxfalls.edu <cartergordon13@gmail.com>
2026-03-10 14:31:04 +0000

    First working WorkSetup.sh

--- SystemSetup/Work/WorkSetup.sh
+++ SystemSetup/Work/WorkSetup.sh
@@ -9,7 +9,7 @@ source ../SystemInstall.sh
 USER="cartergordon"
 USER_EMAIL="carter.gordon@usiouxfalls.edu"
 
-read -p "BitBucket API-Key:" $BIT_API
+read -p "BitBucket API-Key:" BIT_API
 
 # Git Config
 git config user.name $USER
@@ -20,8 +20,9 @@ mkdir -p ~/Code/
 # Install work repositories
 while IFS= read -r repo || [ -n "$repo" ];
 do
-	git clone https://$USER:$BIT_API@bitbucket.org/usiouxfalls/$repo ~/Code/$repo
 
+	git clone https://$USER:$BIT_API@bitbucket.org/usiouxfalls/$repo ~/Code/$repo
+	echo "git clone https://$USER:$BIT_API@bitbucket.org/usiouxfalls/$repo ~/Code/$repo"
 	echo "Installed $repo!"
-done < ../pkg-lists/
+done < ../pkg-lists/work.txt
 
