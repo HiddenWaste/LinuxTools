f1ceb3ddc76cdbf8e40b92e7c368e13fd13f663d
cartergordon <carter.gordon@usiouxfalls.edu>
2026-03-17 15:15:41 +0000

    Moved webadmin docker setup to separate bitbucket repo

--- SystemSetup/web-ad-dock/refresh.sh
+++
@@ -1,6 +0,0 @@
-sudo docker rm -f ubuntu-php-env
-
-sudo docker compose up -d --build # adding --build rebuilds each time
-
-# enter the cli again
-sudo docker exec -it ubuntu-php-env bash
--- SystemSetup/web-ad-dock/start.sh
+++
@@ -1,3 +0,0 @@
-sudo docker compose up -d
-
-sudo docker exec -it ubuntu-php-env bash
